923b290d88bed4391da5a922b35f7f8b3b344509
Trung Tinh <trantrungtinh4954@gmail.com>
2017-11-16 22:29:05 +0700

    add firebase

--- App_Phone_Staff/index.js
+++ App_Phone_Staff/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const parser = require('body-parser').urlencoded({extended: false});
-const request = require('request');
 const app = express();
+// const server = require('http').Server();
+// const io = require('socket.io')(server);
+
+// config data base
+const db = require('./model/fbConfig');
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -11,7 +15,13 @@ app.get('/', (req , res) => res.render('home'));
 app.post('/order' , parser , (req , res) => {
     const {phone , lat , lng , bike , other} = req.body;
     if(!phone) res.send({error: error.message});
-    console.log(phone , lat , lng , bike , other);
+    // console.log(phone , lat , lng , bike , other);
+    db().ref('users').push({phone , lat , lng , bike , other});
     res.send({message: 'OK'});
-})
+});
 app.listen(3000, () => console.log('Server has been started!'));
+
+// server.listen(3000, () => console.log('Server has been started!'));
+// io.on('connection' , socket => {
+//     // to do
+// });
