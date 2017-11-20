5aef13f81fc9a619843e7522d67647fd3de14e5c
Trung Tinh <trantrungtinh4954@gmail.com>
2017-11-20 11:31:50 +0700

    aaa

--- App_GPS_Staff/model/fbConfig.js
+++ App_GPS_Staff/model/fbConfig.js
@@ -1,11 +1,6 @@
 const {initializeApp , database} = require('firebase');
 const config = {
-  apiKey: "AIzaSyCprRGM8i2vAes_0Fzp-Z5ZeH1iJTaGmkQ",
-  authDomain: "webapp-dd1f5.firebaseapp.com",
-  databaseURL: "https://webapp-dd1f5.firebaseio.com",
-  projectId: "webapp-dd1f5",
-  storageBucket: "webapp-dd1f5.appspot.com",
-  messagingSenderId: "425552308033"
+  // your config database firebase
 };
 initializeApp(config);
 module.exports = database;
